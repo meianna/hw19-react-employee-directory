e4fd1434bccaca00c17386ce3325ba9d34b21ecb
Meianna Oeser <meianna.oeser@gmail.com>
2020-08-09 12:18:16 -0700

    wrote out employee records

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,29 @@
 import React from "react";
 import axios from "axios";
 class Home extends React.Component {
-  state = {};
+  state = {
+    employeeInfo: [],
+  };
   componentDidMount = () => {
     axios
       .get("https://randomuser.me/api/?results=200&nat=us")
       .then((employeeRecord) => {
         console.log(employeeRecord);
+        var employee = employeeRecord.data.results;
+        let employeeInfo = [];
+        for (let i = 0; i < employee.length; i++) {
+          let record = {
+            name: employee[i].name.first + employee[i].name.last,
+            email: employee[i].email,
+            dob: employee[i].dob,
+            phone: employee[i].cell,
+          };
+          employeeInfo.push(record);
+        }
+        this.setState({
+          employeeInfo: employeeInfo,
+        });
+        console.log(this.state.employeeInfo);
       });
   };
   render() {
