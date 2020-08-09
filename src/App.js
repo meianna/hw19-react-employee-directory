c447fc11f590a1b7ee67aeeeb90f765c9a8ce6e4
Meianna Oeser <meianna.oeser@gmail.com>
2020-08-09 12:48:04 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import Home from "./pages/Home.js";
+import Footer from "./components/Footer.js";
 function App() {
   return (
     <div className="App">
       <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center bg-info">&copy; Meianna Oeser, 2020</footer>
+  );
+};
+
+export default Footer;
